7459d45965072f363bb757d077a5b33a346360ae
Artem Borisov <dedsa2002@gmail.com>
2019-03-23 13:26:38 +0530

    lavender: board: Enable full dexpreopt
    * This marginally speeds up A/B OTA postinstall at cost of a
      little bit bigger zip size.
    
    Change-Id: I61e71a20bbfc17998bd0c8bade7ee98faad15524

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -250,16 +250,14 @@ WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
-# Enable dex pre-opt to speed up initial boot
+# Dexpreopt
 ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
       WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_PIC := true
-      ifneq ($(TARGET_BUILD_VARIANT),user)
-        # Retain classes.dex in APK's for non-user builds
-        DEX_PREOPT_DEFAULT := nostripping
-      endif
     endif
+  endif
 endif
 
 # inherit from the proprietary version
